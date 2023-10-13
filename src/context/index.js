081bf9c193ce0a1141b4969f4e276bff5af0949d
adenugbamichael <adenugbamicke@gmail.com>
2023-10-13 17:55:14 +0100

    analytics

---
+++ src/context/index.js
@@ -0,0 +1,27 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import { usePathname, useRouter } from "next/navigation"
+import { useState, useEffect, createContext } from "react"
+
+export const GlobalContext = createContext(null)
+
+export default function GlobalState({ children }) {
+  const [sideBarOpen, setSideBarOpen] = useState(false)
+  const { status } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (
+      status === "unauthenticated" &&
+      pathName.includes("/" || "/products" || "/visitors")
+    )
+      router.push("/unauth-page")
+  }, [status])
+  return (
+    <GlobalContext.Provider value={{ sideBarOpen, setSideBarOpen }}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
